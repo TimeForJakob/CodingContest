b6a20e0a1afd8ce451d6f41320d0c8e1cbbfb623
Nedinic Nikola <3timesn@gmail.com>
2017-10-20 10:11:18 +0200

    Dd
    dd

---
+++ src/at/htlwels/codingcontest/Main.java
@@ -0,0 +1,11 @@
+package at.htlwels.codingcontest;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.next();
+    }
+}
